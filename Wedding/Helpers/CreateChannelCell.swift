8f4f32947edc99c1739f48b85a1ae7ef167a5803
Lauren N. Roth <laurennicoleroth@gmail.com>
2018-04-06 15:26:37 -0400

    Create channel cell

---
+++ Wedding/Helpers/CreateChannelCell.swift
@@ -0,0 +1,17 @@
+//
+//  CreateChannelCell.swift
+//  Wedding
+//
+//  Created by Lauren Nicole Roth on 4/6/18.
+//  Copyright © 2018 Lauren Nicole Roth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateChannelCell: UITableViewCell {
+  
+  @IBOutlet weak var newChannelNameField: UITextField!
+  @IBOutlet weak var createChannelButton: UIButton!
+  
+}
